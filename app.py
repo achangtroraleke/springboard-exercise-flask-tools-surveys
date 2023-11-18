565b8ebc0076a1c07f9c539130f88c9e75201cee
Atachart Changtroraleke <atachart.changtroraleke@gmail.com>
2023-11-18 16:06:55 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, flash, request, render_template, redirect
+from surveys import satisfaction_survey
+
+app = Flask(__name__)
+responses = []
+@app.route('/')
+def home():
+    survey = satisfaction_survey
+    return render_template('home.html', survey=survey)
+
+@app.route('/questions/<index>', methods=["GET","POST"] )
+def getQuestion(index):
+   
+
+    if request.method == "POST":
+        responses.append(request.form['answer'])
+        
+        if int(index) >= len(satisfaction_survey.questions):
+            return redirect('/completed')
+        else:
+            return redirect(f'/questions/{int(index)+1}')
+    else:
+        if len(responses) != int(index)-1:
+            responses.clear()
+            return redirect('/questions/1')
+        else:
+            question = satisfaction_survey.questions[int(index)-1]
+
+            content = {'index':int(index), 'question':question}
+            return render_template('question.html', content=content)
+
+@app.route('/completed')
+def completed():
+    print(responses)
+    return render_template('thanks.html')
+
+
+if __name__  == "__main__":
+    app.run(debug=True)
